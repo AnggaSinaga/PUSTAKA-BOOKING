cbfc6bb55e264dc630927f112cedb57c0020c56a
AnggaSaputra <layybatak32@gmail.com>
2023-04-13 16:12:26 +0700

    pesan commit atau perubahan

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,21 +2,21 @@
  <h1><?php echo $judul ?></h1>
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Raden Ahmad</li>
- <li>Nama Belakang : Nashieh Hamied</li>
+ <li>Nama Depan : Angga</li>
+ <li>Nama Belakang : Saputra</li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Jalan Cibarusah</li>
+ <li> Jalan Serang Baru</li>
  </ul>
 
  <h4>Tempat Lahir</h4>
  <ul type="none">
- <li>Bekasi</li>
+ <li>Jakarta</li>
  </ul>
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
- <li>Bulutangkis</li>
+ <li>Sepak Bola</li>
  </ul>
 </section>
