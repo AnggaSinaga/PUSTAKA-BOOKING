ad71291723e23c0e85d612e7c67b587f5cffaf17
AnggaSaputra <layybatak32@gmail.com>
2023-04-13 15:29:29 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Angga Saputra, Saya berkuliah di dearah cikarang dan saya kuliah Universitas Bina Sarana Informatika
+        Kelas 17.6A.24 
+        NIM 17200648";
+    }
+}
